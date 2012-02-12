7d9716515da5df3795fd90339ef3c892c59c210c
Roman Gonzalez <romanandreg@gmail.com>
2012-02-12 01:47:06 -0800

    Bumping dependency version

--- project.clj
+++ project.clj
@@ -2,4 +2,4 @@
   :description "JSON zetta-parser combinator (port of Haskell's aeson)"
   :author "Roman Gonzalez"
   :dependencies [[org.clojure/clojure "1.3.0"]
-                 [org.van-clj/zetta-parser "0.0.1-SNAPSHOT"]])
+                 [org.van-clj/zetta-parser "0.0.2"]])
