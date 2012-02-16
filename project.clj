09f1fece6f0f9458c2e13077905c8368d2714011
Roman Gonzalez <romanandreg@gmail.com>
2012-02-15 19:54:22 -0800

    Removing SNAPSHOT from version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.van-clj/json-zetta "0.0.1-SNAPSHOT"
+(defproject org.van-clj/json-zetta "0.0.1"
   :description "JSON zetta-parser combinator (port of Haskell's aeson)"
   :author "Roman Gonzalez"
   :dependencies [[org.clojure/clojure "1.3.0"]
