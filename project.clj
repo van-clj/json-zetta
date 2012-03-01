31ffc0acb98c0c97c276364a7bed7b0beaab87d6
Roman Gonzalez <romanandreg@gmail.com>
2012-02-29 23:44:35 -0800

    Bumping to version 0.0.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.van-clj/json-zetta "0.0.3-SNAPSHOT"
+(defproject org.van-clj/json-zetta "0.0.3"
   :description "JSON zetta-parser combinator (port of Haskell's aeson)"
   :author "Roman Gonzalez"
   :warn-on-reflection true
